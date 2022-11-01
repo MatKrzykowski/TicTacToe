a9f233b01cf9c45349e40d131f21dbe44f396e4b
MKrzykow <matkrzykowski@gmail.com>
2022-11-01 13:29:27 +0100

    Add board printout

--- board.py
+++ board.py
@@ -6,7 +6,7 @@ class TicTacTor_field(enum.Enum):
     X = 2
 
 class TicTacToe_board():
-    def __init__(self, array):
+    def __init__(self, array=[TicTacTor_field._ for i in range(9)]):
         self.array = array
 
     def __getitem__(self, key):
@@ -14,3 +14,26 @@ class TicTacToe_board():
 
     def __setitem__(self, key, new_value):
         self.array[key] = new_value
+
+    def symbol(self, i):
+        if self[i] == TicTacTor_field._:
+            return " "
+        elif self[i] == TicTacTor_field.X:
+            return "X"
+        elif self[i] == TicTacTor_field.O:
+            return "O"
+        else:
+            assert False
+
+    def print_line(self, i):
+        return f"{self.symbol(i*3)} | {self.symbol(i*3+1)} | {self.symbol(i*3+2)}"
+
+    def __str__(self):
+        return f"""
+           1   2   3
+        A  {self.print_line(0)}
+           ---------
+        B  {self.print_line(1)}
+           ---------
+        C  {self.print_line(2)}
+        """
