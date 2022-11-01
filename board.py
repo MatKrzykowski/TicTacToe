b1bbf6ffd9107719f6ef8d7c633f74d4b81ff2ad
MKrzykow <matkrzykowski@gmail.com>
2022-11-01 14:03:04 +0100

    Move ASCII repr of board fields to a dict

--- board.py
+++ board.py
@@ -1,10 +1,19 @@
 import enum
 import numpy as np
 
+
 class TicTacTor_field(enum.Enum):
     _ = 0
     O = 1
     X = 2
+
+
+symbol = {
+    TicTacTor_field._: " ",
+    TicTacTor_field.X: "X",
+    TicTacTor_field.O: "O",
+}
+
 
 class TicTacToe_board():
     def __init__(self, array=[TicTacTor_field._ for i in range(9)]):
@@ -17,14 +26,7 @@ class TicTacToe_board():
         self.array[key] = new_value
 
     def symbol(self, i):
-        if self[i] == TicTacTor_field._:
-            return " "
-        elif self[i] == TicTacTor_field.X:
-            return "X"
-        elif self[i] == TicTacTor_field.O:
-            return "O"
-        else:
-            assert False
+        return symbol[self[i]]
 
     def print_line(self, i):
         return f"{self.symbol(i*3)} | {self.symbol(i*3+1)} | {self.symbol(i*3+2)}"
